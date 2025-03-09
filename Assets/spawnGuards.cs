5cf460eceb4df40e5160555083ae83a28e2f027b
Stylianos Regas <styliregas@gmail.com>
2025-03-09 17:26:44 -0500

    Finalized guards spawining
    Now the guards will spawn when the player gets the key

---
+++ Assets/spawnGuards.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnGuards : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject guards;
+    public bool hasPotion;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasPotion)
+        {
+
+            guards.SetActive(true);
+        }
+
+        else
+        {
+            guards.SetActive(false);
+            Debug.Log("Potion is not in the scene");
+        }
+
+        
+
+    }
+}
