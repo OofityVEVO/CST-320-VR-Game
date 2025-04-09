5f9d06d6b09d4ea4e1ca42cc610b1ae6e55330f9
Stylianos Regas <styliregas@gmail.com>
2025-04-09 00:06:31 -0700

    did some of the audio
    Help

---
+++ Assets/EntranceDialogue1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceDialogue1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public DialogueScene audios;
+    void Start()
+    {
+        audios.PlayAudio();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
